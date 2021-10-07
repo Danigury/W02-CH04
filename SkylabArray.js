887ed688fd9ac52ee8b74376a04fb7d5774eeb74
Dani Sanchez <danysq87@gmail.com>
2021-10-07 22:36:06 +0200

    Posh test passed :)

--- SkylabArray.js
+++ SkylabArray.js
@@ -7,10 +7,18 @@ class SkylabArray {
       this.length++;
     }
   }
+  posh(...obj) {
+    for (let i = 0; i < obj.length; i++) {
+      this[this.length] = obj[i];
+      this.length++;
+    }
+    return this;
+  }
 }
 
 const test = new SkylabArray(5, "ok", 7);
 
 console.log(test);
+console.log(test.posh("paco", "paca", "paqui", "paquirrín"));
 
 module.exports = { SkylabArray };
